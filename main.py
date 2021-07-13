b03801529327c634b064055670ac1d74b3700a3d
triacontakai <triacontakai@github.com>
2021-07-13 18:09:50 -0400

    updated main.py with testing for IrcHandler

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import os
+import asyncio
+import typing
+from connector import IrcHandler
 
 # load settings from environment variables
 host = os.getenv('IRC_HOST')
 port = int(os.getenv('IRC_PORT'))
 username = os.getenv('IRC_USERNAME')
 password = os.getenv('IRC_PASSWORD')
+
+# receive messages from irc connection and send off to process in other functions
+async def main():
+    async with IrcHandler(host, port) as client:
+        while True:
+            await client.login(username, password)
+            print('receiving message')
+            msg = await client.recv_msg()
+            print('received message')
+            print(msg)
+
+asyncio.run(main())
